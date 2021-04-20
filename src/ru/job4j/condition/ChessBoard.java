15646e7956b5bc7ef9466f85292af99d04c9c7e6
icevice <Fo911624>
2021-04-20 16:01:25 +0700

    4.3.6. Math.abs [#473912] v2

--- src/ru/job4j/condition/ChessBoard.java
+++ src/ru/job4j/condition/ChessBoard.java
@@ -2,7 +2,6 @@ package ru.job4j.condition;
 
 public class ChessBoard {
     public static int way(int x1, int y1, int x2, int y2) {
-
         int rsl = 0;
 
         if (x1 < 0 || y1 < 0 || x2 < 0 || y2 < 0
