3a340fe666d3e8e70837610712ed21dde0b6b29b
icevice <Fo911624>
2021-07-08 20:53:14 +0700

    5.7. Ипотека [#156315]

---
+++ src/ru/job4j/loop/Mortgage.java
@@ -0,0 +1,15 @@
+package ru.job4j.loop;
+
+public class Mortgage {
+    public static int year(double amount, double salary, double percent) {
+        int year = 0;
+        do {
+            amount = amount + amount * percent / 100;
+            amount -= salary;
+            year++;
+        } while (amount > 0);
+        return year;
+    }
+
+
+}
